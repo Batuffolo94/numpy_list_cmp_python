fdc16b2c393366adcc237d9286f2c6cd6c05d64f
Batuffolo94 <113693707+Batuffolo94@users.noreply.github.com>
2022-11-15 01:15:12 -0300

    Add files via upload

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -19,13 +19,14 @@ if __name__ == '__main__':
     # (len) de un string pasado como parámetro
 
     # len_string = lambda......
-
+    len_string = lambda x:int(x)
+    print(len_string(4))
     # 2)
     # Lista de string
     palabras = ['Inove', 'casa', 'programacion']
 
     # Utilice la función map para mapear una lambda expression
-    # que retorne el tamaño (len) de cada texto em cata iteración
+    # que retorne el tamaño (len) de cada texto en cada iteración
     # de la lista de textos
     # El resultado (el len de cada palabra) se debe ir almacenando
     # en una nueva lista
@@ -36,4 +37,6 @@ if __name__ == '__main__':
 
     # palabras_len = list(map....)
 
+    palabras_len = list (map(lambda x:len(x),palabras))
+    print (palabras_len)
     print("terminamos")
